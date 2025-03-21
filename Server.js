8c0f74c2031fb13cc3468b25affbaf598f0a1506
lavanya0926 <lavanyaramireddy2609@gmail.com>
2025-03-21 20:02:01 +0530

    first commit

---
+++ Server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const connectDB = require('./config/db'); 
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+
+connectDB();
+
+
+app.use(express.json({ extended: false }));
+app.use(cors());
+
+
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/authLogin', require('./routes/authLogin')); 
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
